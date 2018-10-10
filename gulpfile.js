c97ed268a98d7cd017554a718761cbe6f2d7ade0
Ryota Konno <rkonno.rb@gmail.com>
2018-10-10 22:43:20 +0900

    Npm install postcss-preset-env

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ const gulp = require('gulp');
 gulp.task('css', () => {
     const postcss       = require('gulp-postcss');
     const sourcemaps    = require('gulp-sourcemaps');
-    const autoprefixer  = require('autoprefixer');
-    const cssnano       = require('cssnano');
 
     return gulp.src('extension/css/*.css')
         .pipe( sourcemaps.init() )
-        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( postcss([
+            require('postcss-preset-env'),
+            require('autoprefixer'),
+            require('cssnano')
+        ]) )
         .pipe( sourcemaps.write('.') )
         .pipe( gulp.dest('build/') )
 });
