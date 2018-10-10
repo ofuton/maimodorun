ea2a4f7407acc5aa2601807b377844cdd2474763
Ryota Konno <ryota-konno@cybozu.co.jp>
2018-10-10 19:54:38 +0900

    Npm install cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 const gulp = require('gulp');
 
 gulp.task('css', () => {
-    const postcss       = require('gulp-postcss')
-    const sourcemaps    = require('gulp-sourcemaps')
-    const autoprefixer  = require('autoprefixer')
+    const postcss       = require('gulp-postcss');
+    const sourcemaps    = require('gulp-sourcemaps');
+    const autoprefixer  = require('autoprefixer');
+    const cssnano       = require('cssnano');
 
     return gulp.src('extension/css/*.css')
         .pipe( sourcemaps.init() )
-        .pipe( postcss([ autoprefixer() ]) )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
         .pipe( sourcemaps.write('.') )
         .pipe( gulp.dest('build/') )
-})
+});
