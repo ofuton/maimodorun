03bf210d75a495418390952dfb026c1ae838d4aa
Keisuke Emi <emiksk0910@gmail.com>
2019-02-04 03:36:45 +0900

    Support people page to save and recovery text

--- src/js/utils/detect-page.js
+++ src/js/utils/detect-page.js
@@ -3,11 +3,17 @@ const getHashName = () => location.hash.replace(/^#[/]/g, '');
 
 const isThread = hashName => /^space\/\d+\/thread\/\d+(\/\d+)?$/.test(hashName);
 
+const isPeople = hashName => /^people\/user\/[^/]+(\/\d+)?$/.test(hashName);
+
 const detectPage = () => {
     const hashName = getHashName();
 
     if (isThread(hashName)) {
         return 'thread';
+    }
+
+    if (isPeople(hashName)) {
+        return 'people';
     }
 
     return 'unknown';
