e6846eb132fd3e281b3d0e2bbbd962e5ab1f6290
Keisuke Emi <emiksk0910@gmail.com>
2019-02-07 22:58:53 +0900

    Add detect function for record

--- src/js/utils/detect-page.js
+++ src/js/utils/detect-page.js
@@ -1,5 +1,8 @@
+import parseQuery from 'utils/parse-query';
+const getPathName = () => location.pathname.replace(/^[/]?/g, '');
+
 // Drops leading hash and slash
-const getHashName = () => location.hash.replace(/^#[/]/g, '');
+const getHashName = () => location.hash.replace(/^#[/]?/g, '');
 
 const isThread = hashName => /^space\/\d+\/thread\/\d+(\/\d+)?$/.test(hashName);
 
@@ -7,7 +10,16 @@ const isPeople = hashName => /^people\/user\/[^/]+(\/\d+)?$/.test(hashName);
 
 const isMessage = hashName => /^message\/\d+;\d+(\/\d+)?$/.test(hashName);
 
+const isRecord = (pathName, hashName) => {
+    return isAppPath(pathName) && includeRecordId(hashName);
+};
+
+const isAppPath = (pathName) => /^k\/\d+\/show$/.test(pathName);
+
+const includeRecordId = (hashName) => parseQuery(hashName).hasOwnProperty('record');
+
 const detectPage = () => {
+    const pathName = getPathName();
     const hashName = getHashName();
 
     if (isThread(hashName)) {
@@ -20,6 +32,10 @@ const detectPage = () => {
 
     if (isMessage(hashName)) {
         return 'message';
+    }
+
+    if (isRecord(pathName, hashName)) {
+        return 'record';
     }
 
     return 'unknown';
